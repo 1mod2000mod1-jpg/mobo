f18d388d568c15862c32c68040fbc3f2bb6edf90
1mod2000mod1-jpg <1mod2000mod1@gmail.com>
2025-09-28 21:51:17 +0100

    Update WormGPT.py

--- WormGPT.py
+++ WormGPT.py
@@ -272,7 +272,7 @@ def handle_all_messages(message):
     # معالجة الرسالة باستخدام الذكاء الاصطناعي
     try:
         txt = message.text
-        res = requests.get(f"https://sii3.top/api/DarkCode.php?text=hello{txt}", timeout=10)
+        res = requests.get(f"http://fi8.bot-hosting.net:20163/elostoracode?text=hello{txt}", timeout=10)
         res.raise_for_status()
         data = res.json()
         response = data.get("response", "❌ لا يوجد رد من الخادم")
